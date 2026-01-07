b46527d397641588fd9efedb5a16ebcd6a022e54
Harshita-Khandate <harshitakhandate@gmail.com>
2026-01-07 22:47:04 +0530

    add Expense is done nowww

---
+++ PersonalFinanceTracker/Controllers/ExpenseController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PersonalFinanceTracker.DataBase;
+using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ExpenseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // 🔁 COMMON METHOD
+        private void LoadCategories()
+        {
+            ViewBag.catlist = new SelectList(
+                _context.ExpenseCategory.ToList(),
+                "ExpenseCategoryID",
+                "CategoryName"
+            );
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("UserID") == null)
+            {
+                return RedirectToAction("Signin", "Home");
+            }
+
+            LoadCategories();
+            return View(new Expense()); // ✅ IMPORTANT
+        }
+
+        [HttpPost]
+        public IActionResult Index(Expense e)
+        {
+            int? userId = HttpContext.Session.GetInt32("UserID");
+            if (userId == null)
+            {
+                return RedirectToAction("Signin", "Home");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                LoadCategories();   // ✅ MUST
+                return View(e);
+            }
+
+            e.UserID = userId.Value;
+           // e.ExpenseDate = DateTime.Now;
+
+            _context.Expense.Add(e);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
